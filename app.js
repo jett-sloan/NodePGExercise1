38cc69ed38e362f78b4ba5aef31b5c27e94b4bd2
jett-sloan <jettsloansloan@gmail.com>
2024-04-12 15:57:04 -0400

    second commit

--- app.js
+++ app.js
@@ -2,35 +2,39 @@ const express = require("express");
 const app = express();
 const db = require('./db')
 const ExpressError = require("./error");
-app.use(function (req, res, next) {
-    const err = new ExpressError("Not Found", 404);
-    return next(err);
-  });
-  
-  /** general error handler */
-  
-  app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-  
-    return res.json({
-      error: err,
-      message: err.message
-    });
-  });
+app.use(express.json());
+//app.use(function (req, res, next) {
+//    const err = new ExpressError("Not Found", 404);
+//    return next(err);
+//  });
+//  
+//  /** general error handler */
+//  
+//  app.use((err, req, res, next) => {
+//    res.status(err.status || 500);
+//  
+//    return res.json({
+//      error: err,
+//      message: err.message
+//    });
+//  });
   
 const port = 3000 
 // Parse request bodies for JSON
-app.use(express.json());
 
 app.get("/companies", async function (req,res,next){
     const results = await db.query(
-        `SELECT * FROM companies`);
+        `SELECT * FROM companies c
+        JOIN industriescompany ic
+         ON c.code = ic.company_code
+        JOIN industries i ON i.code = ic.industry_code   `);
         console.log(results)
 
   return res.json(results.rows);
 
 
 });
+
 
 
 app.post("/companies", async function(req,res,next){
@@ -107,16 +111,31 @@ app.post("/invoices",async function(req,res,next){
 })
 
 
+
 app.put("/invoices/:id", async function(req,res,next){
    
-    let id = req.params.id
+    const {id} = req.params
     if(id.length === 0) throw new ExpressError("Not found!", 404);
-    const {amt} = req.body 
+    const {amt,paid} = req.body
+    const result = await db.query(
+        `SELECT paid_date FROM invoices WHERE id=$1`,[id]
+    )
+    let paid_date = result.rows[0]
+    if(paid){
+        let nowDate = new Date(); 
+        paid_date = nowDate.getFullYear()+'/'+(nowDate.getMonth()+1)+'/'+nowDate.getDate(); 
+    }else if(!paid){
+        paid_date = null
+    }else{
+
+    }
     let results = await db.query(
-        `UPDATE invoices SET amt=$1
-        WHERE id = $2
+        `UPDATE invoices SET amt=$1,
+        paid =$2,
+        paid_date =$4
+        WHERE id = $3
         RETURNING id,comp_code,amt,paid,add_date,paid_date`,
-     [amt,id]
+     [amt,paid,id,paid_date]
     )
     console.log(results.rows)
     return res.json(results.rows)
@@ -165,32 +184,38 @@ app.get("/companies/:code", async function(req,res,next){
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/industries", async function(req,res,next){
+    try{
+    const results = await db.query(
+        `SELECT 
+        i.code AS industry_code,
+        i.industry,
+        c.code AS company_code,
+        c.name AS company_name
+    FROM 
+        industries i
+    JOIN 
+        industriescompany ic ON i.code = ic.industry_code
+    JOIN 
+        companies c ON ic.company_code = c.code;`
+    )
+    return res.json(results.rows);}
+    catch(err){
+        return next(err)
+    }
+})
+app.post("/industries", async function(req,res,next){
+    try{
+    let {code, industry} = req.body
+    const results = await db.query(
+        `INSERT INTO industries (code,industry) VALUES ($1,$2)
+        RETURNING code, industry`,[code, industry]
+    )
+    return res.json(results.rows)
+    }catch(err){
+        return next(err)
+    }
+})
 
 app.listen(port,()=>{
     console.log('sever on')
